04a46b104707ca2e74ad4e29793cfaa25748c323
i8x <steinhebelal@ornl.gov>
2026-01-21 09:56:02 -0500

    Add argparse option to run diagnostics

--- neuropix/analysis_processing/root_to_hdf5/withRoot.py
+++ neuropix/analysis_processing/root_to_hdf5/withRoot.py
@@ -11,8 +11,6 @@
 import sys, argparse
 import functions
 
-import h5py
-
 #################
 # methods
 #################
@@ -36,15 +34,17 @@ def main(args):
         print("Unknown input file type")
         sys.exit()
 
-    #test output hdf5
-    fin = h5py.File(f'{args.fileIn[:-5]}.hdf5', 'r')
-    print(fin.keys())
-    print(fin['metadata'].keys())
-    print(fin['data'].keys())
-    for i in fin['metadata'].keys():
-        print(fin['metadata'][i][()])
-    for j in fin['data'].keys():
-        print(fin['data'][j][()])
+    if args.diagnostics:
+        import h5py
+        #test output hdf5 by printing to terminal
+        fin = h5py.File(f'{args.fileIn[:-5]}.hdf5', 'r')
+        print(fin.keys())
+        print(fin['metadata'].keys())
+        print(fin['data'].keys())
+        for i in fin['metadata'].keys():
+            print(fin['metadata'][i][()])
+        for j in fin['data'].keys():
+            print(fin['data'][j][()])
 
 #################
 # call to main
@@ -53,10 +53,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(prog='rootToHdf5',
         description='Convert ROOT output from Allpix-squared to HDF5 file with only NEUROPix-relevant variables retained')
-    parser.add_argument('-f', '--fileIn', default='./gamma_50kev_100k.root')    
-    parser.add_argument('-o', '--dirOut', default='./')  
-    parser.add_argument("-l", '--libAllpixObjects', required=False,
-                    help="specify path to the libAllpixObjects library (generally in allpix-squared/lib/)) ")
+    parser.add_argument('-f', '--fileIn', required=True, help="Path to input ROOT file. Only accepts one file at a time.")    
+    parser.add_argument('-o', '--dirOut', default='./', help="Path to output directory where HDF5 will be saved. Defaults to directory where this script lives.")  
+    parser.add_argument('-l', '--libAllpixObjects', required=False, help="Path to the libAllpixObjects library (generally in allpix-squared/lib/)) ")
+    parser.add_argument('-d','--diagnostics', required=False, action='store_true', help="If given, will run diagnostics on the output HDF5 to confirm that it was created and populated as expected.")
 
     args = parser.parse_args()
     args.dirOut = functions.outputCheck(args.dirOut)
